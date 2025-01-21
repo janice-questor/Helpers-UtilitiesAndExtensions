ca07332531a0149e3f9717f957a0a2f133b7b762
Marcelo <6012333+marcelo-unimake@users.noreply.github.com>
2025-01-21 08:37:10 -0300

    test: False base64 result. ID #169859

---
+++ source/Unimake.Test/Validators/Base64Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace Unimake.Helpers_UtilitiesAndExtensions.Test.Validators
+{
+    public class Base64Test
+    {
+        #region Public Methods
+
+        [Fact]
+        public void IsBase64()
+        {
+            var base64 = "SGVsbG8gV29ybGQ=";
+            Assert.True(base64.IsBase64());
+
+            base64 = "LVT5DvkYQEDudrs9dvrd";
+            Assert.False(base64.IsBase64());//false, a string LVT5DvkYQEDudrs9dvrd retorna verdade, quando não deveria
+        }
+
+        #endregion Public Methods
+    }
+}
